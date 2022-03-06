1097abeb4f34c897d3c785d217f78f3a6e93f6e2
leeseungmin0 <tmdalsdl112@naver.com>
2022-03-06 18:35:07 +0900

    css cake font color change

---
+++ main.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let container = document.querySelector('.container')
+let homeContainer = document.querySelector('.home__container')
+let slide = document.querySelectorAll('.home__slide');
+let count = slide.length
+console.log(homeContainer)
