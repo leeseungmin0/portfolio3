ce2801c1c1343ce0b5f7d93df28e038ef56a409f
ezdar2743@naver.com <ezdar2743@gmail.com>
2022-03-06 18:46:50 +0900

    Change pic position

--- main.js
+++ main.js
@@ -3,5 +3,11 @@
 let container = document.querySelector('.container')
 let homeContainer = document.querySelector('.home__container')
 let slide = document.querySelectorAll('.home__slide');
-let count = slide.length
-console.log(homeContainer)
+let count = slide.length;
+let currentIdx = 0;
+
+for(let i=0; i<count; i++){
+    slide[i].style.left = i*100 +'%'
+    currentIdx=i
+}
+
